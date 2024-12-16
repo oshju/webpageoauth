95911ebdfe7995d166348ed763d3fc84e8eec062
oshju <87846045+oshju@users.noreply.github.com>
2024-12-16 14:24:03 +0100

    Add files via upload

---
+++ <html>.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirigiendo...</title>
+    <script type="text/javascript">
+        // Redirige al esquema personalizado
+        window.location.href = "myapp://auth";
+    </script>
+</head>
+<body>
+    <p>Redirigiendo a la aplicación...</p>
+    <p>Si la redirección no funciona, <a href="myapp://auth">haz clic aquí</a>.</p>
+</body>
+</html>
